eebb73e412e1f1a8309ad0ad4aeb5f06faf14df0
chainrocker55 <chainrock55@gmail.com>
2019-08-29 19:41:01 +0700

    Change binding

--- app/src/main/java/buu/informatics/s59160545/parking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160545/parking/MainActivity.kt
@@ -7,148 +7,68 @@ import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160545.parking.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private val park1:ParkCar = ParkCar()
-    private val park2:ParkCar = ParkCar()
-    private val park3:ParkCar = ParkCar()
-    private  var flagCar:Number = 1
+    private val park1:ParkCar = ParkCar(0)
+    private val park2:ParkCar = ParkCar(1)
+    private val park3:ParkCar = ParkCar(2)
+    private  val park = arrayOf(park1,park2,park3)
+    private  var flagCar = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         binding.apply {
+            cars = this@MainActivity.park
             carone.setOnClickListener {
-                SelectCarOne(it)
+                SelectPark(it)
             }
             cartwo.setOnClickListener {
-                SelectCarTwo(it)
+                SelectPark(it)
             }
             cartree.setOnClickListener {
-                SelectCarTree(it)
+                SelectPark(it)
             }
             delete.setOnClickListener {
                 Delete(it)
             }
-            update.setOnClickListener {
-                Update(it)
+            save.setOnClickListener {
+                Save(it)
             }
-            selected.text = "Selected : "+flagCar
-
         }
     }
-
-    private fun SelectCarOne(view:View) {
-        flagCar = 1
-
+    private fun getCarIdFormView(view:View):Int {
+        when(view.id) {
+            R.id.carone -> return 0
+            R.id.cartwo -> return 1
+            R.id.cartree -> return 2
+        }
+        return -1
+    }
+    private fun SelectPark(view: View) {
         binding.apply {
-            selected.text = "Selected : "+flagCar
-            number.setText(park1.number)
-            model.setText(park1.model)
-            name.setText(park1.name)
-        }
-
-    }
-    private fun SelectCarTwo(view: View){
-        flagCar = 2
-
-        binding.apply {
-            selected.text = "Selected : "+flagCar
-            number.setText(park2.number)
-            model.setText(park2.model)
-            name.setText(park2.name)
+            flagCar = getCarIdFormView(view)
+            car = park?.get(flagCar)
+            invalidateAll()
         }
     }
-    private fun SelectCarTree(view: View){
-        flagCar = 3
 
-        binding.apply {
-            selected.text = "Selected : "+flagCar
-            number.setText(park3.number)
-            model.setText(park3.model)
-            name.setText(park3.name)
-        }
-    }
     private  fun Delete(view:View){
-        if (flagCar==1){
-            park1.number=""
-            park1.model=""
-            park1.name=""
-            binding.carone.text = "Free"
-            binding.carone.setBackgroundColor(Color.GREEN)
-            ResetText()
-        }else if(flagCar==2){
-            park2.number=""
-            park2.model=""
-            park2.name=""
-            binding.cartwo.text = "Free"
-            binding.cartwo.setBackgroundColor(Color.GREEN)
-            ResetText()
-        }else{
-            park3.number=""
-            park3.model=""
-            park3.name=""
-            binding.cartree.text = "Free"
-            binding.cartwo.setBackgroundColor(Color.GREEN)
-            ResetText()
-        }
+        val car = park?.get(flagCar)
+        car.empty = true
+        car.number = ""
+        car.model = ""
+        car.name = ""
+        binding.invalidateAll()
     }
-    private  fun Update(view:View){
-        if (flagCar==1 &&
-            park1.number.isBlank()&&
-            park1.model.isBlank()&&
-            park1.name.isBlank())
-        {
-            binding.apply {
-                park1.number = number.text.toString()
-                park1.model = model.text.toString()
-                park1.name = name.text.toString()
-                carone.text = "Not Free"
-                carone.setBackgroundColor(Color.RED)
-
-            }
-            Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
-
-        }else if(flagCar==2 &&
-            park2.number.isBlank()&&
-            park2.model.isBlank()&&
-            park2.name.isBlank())
-        {
-            binding.apply {
-                park2.number = number.text.toString()
-                park2.model = model.text.toString()
-                park2.name = name.text.toString()
-                cartwo.text = "Not Free"
-                cartwo.setBackgroundColor(Color.RED)
-            }
-            Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
-        }else if(   park3.number.isBlank()&&
-                    park3.model.isBlank()&&
-                    park3.name.isBlank())
-
-        {
-            binding.apply {
-                park3.number = number.text.toString()
-                park3.model = model.text.toString()
-                park3.name = name.text.toString()
-                cartree.text = "Not Free"
-                cartree.setBackgroundColor(Color.RED)
-            }
-            Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(applicationContext,"Not Free",Toast.LENGTH_SHORT).show()
-        }
+    private  fun Save(view:View){
+        val car = park?.get(flagCar)
+        car.empty = false
+        binding.invalidateAll()
     }
-    private fun ResetText(){
-        binding.apply {
-            number.setText("")
-            model.setText("")
-            name.setText("")
-        }
-    }
+
 
 
 }
